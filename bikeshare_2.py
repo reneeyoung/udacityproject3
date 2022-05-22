8d39020b64774567d9750cf6cdc08978345bcd9e
reneeyoung <104277473+reneeyoung@users.noreply.github.com>
2022-05-22 16:05:12 -0500

    Refactor: updating code lines 139 through 252
    Refactor: updating code lines 139 through 252
    
    Added lines 139 through 252 to run loops and correct outputs,

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -135,3 +135,120 @@ def main():
 
 if __name__ == "__main__":
 	main()
+
+ # TO DO: display most commonly used end station
+    common_end_station= df['End Station'].mode()[0]
+    print("most common end station is {}".format(common_end_station))
+
+    # TO DO: display most frequent combination of start station and end station trip
+    df['combination']=df['Start Station']+" "+"to"+" "+ df['End Station']
+    frequent_combination= df['combination'].mode()[0]
+    print("most frequent combination of start station and end station is {} ".format(frequent_combination))
+
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+
+def trip_duration_stats(df):
+    """Displays statistics on the total and average trip duration."""
+
+    print('\nCalculating Trip Duration...\n')
+    start_time = time.time()
+
+    # TO DO: display total travel time
+    total_travel_time=df['Trip Duration'].sum()
+    minute,second=divmod(total_travel_time,60)
+    hour,minute=divmod(minute,60)
+    print("total travel time: {} hours, {} minutes, and {} seconds".format(hour,minute,second))
+
+
+    # TO DO: display mean travel time
+    mean_travel_time=df['Trip Duration'].mean()
+    minute,second=divmod(mean_travel_time,60)
+    hour,minute=divmod(minute,60)
+    print("mean travel time: {} hours, {} minutes, and {} seconds".format(hour,minute,second))
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+
+def user_stats(df):
+    """Displays statistics on bikeshare users."""
+
+    print('\nCalculating User Stats...\n')
+    start_time = time.time()
+
+    # TO DO: Display counts of user types
+    user_type_counts= df['User Type'].value_counts()
+    print("counts of users types are:\n",user_type_counts)
+
+
+    # TO DO: Display counts of gender
+    try:
+        gender = df['Gender'].value_counts()
+        print("count of user by gender:\n", gender)
+    except:
+        print("no gender available")
+        
+
+    # TO DO: Display earliest, most recent, and most common year of birth
+    try:
+        earliest= int(df['Birth Year'].min())
+        print("\nearliest user or oldest is from year",earliest)
+    except: 
+        print("no birth year available")
+    
+    try:
+        most_recent= int(df['Birth Year'].max())
+        print("most recent or youngest user is from year",most_recent)
+    except: 
+        print("no birth year available")
+    
+    try:
+        most_common= int(df['Birth Year'].mode()[0])
+        print("most users are born in",most_common)
+    except:
+        print("no birth year available")
+    
+    
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+def display_raw_data(df):
+    print(df.head())
+    next = 0
+    while True:
+        view_raw_data = input('\nWould you like to view 5 rows of individual trip data? Enter yes or no.\n')
+        if view_raw_data.lower() != 'yes':
+            return
+        next = next + 5
+        print(df.iloc[next:next+5])
+
+def main():
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
+
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df)
+
+        while True:
+            view_raw_data = input('\nWould you like to view 5 rows of individual trip data? Enter yes or no.\n')
+            if view_raw_data.lower() != 'yes':
+                break
+            display_raw_data(df)
+            break
+        
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
+
+
+if __name__ == "__main__":
+	main()
+
+	
